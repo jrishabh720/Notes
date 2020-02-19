0cd9e1828bdb975560d969e54e5a6858826c3a68
Rishabh Jain <jrishabh720@gmail.com>
2020-02-20 00:26:14 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
+const chalk = require('chalk');
 
 const addNote = function(title,body){
 	const notes = loadNote();
 	const dublicate = notes.filter(function(note){
 		return note.title==title;
 	})
-	if(dublicate.length==0)
+	if(dublicate.length==0){
 	notes.push({
 		title:title,
 		body:body
-	})else{
+	})}else{
 		console.log("Already Exists");
 	}
 	savenote(notes)
@@ -26,8 +27,18 @@ const loadNote = function(){
 	}
 }
 
+const remove = function(title){
+	const notes = loadNote();
+	const need = notes.filter(function(note){
+		return note.title !== JSON.stringify(title);
+	})
+	if(need.length <  notes.length){console.log(chalk.green.inverse(" Removed "));}
+	else{console.log(chalk.red.inverse(" Not Founded "))};
+	savenote(need);
+}
 
 module.exports = {
 	addNote : addNote,
-	loadNote : loadNote
+	loadNote : loadNote,
+	remove : remove
 }
